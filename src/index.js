b581dc04c620601ff2726b03a4fe799e582e1f8d
shufu420 <122318862+shufu420@users.noreply.github.com>
2023-05-17 09:01:21 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ import { createServer as createHttpServer } from "node:http";
 import { readFileSync, existsSync } from "node:fs";
 import serveStatic from "serve-static";
 
-// The following message MAY NOT be removed
-console.log("Hypertabs\nThis program comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it\nunder the terms of the GNU General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version.\n\nYou should have received a copy of the GNU General Public License\nalong with this program. If not, see <https://www.gnu.org/licenses/>.\n");
-
 const bare = createBareServer("/bare/");
 const serve = serveStatic(fileURLToPath(new URL("../static/", import.meta.url)), { fallthrough: false });
 var server;
